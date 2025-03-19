9bde1d048195f24a9820a97f0a7177a1d0693dca
fr <75056113+FrenzY8@users.noreply.github.com>
2025-03-20 05:58:20 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-apt update -y && apt full-upgrade -y
-apt-get install -y nodejs git
-
-git clone https://github.com/FaizBastomi/termux-fetish-download "fetish"
-cd fetish
-npm install
-
-echo "[!] All Done, you can do cd fetish and run npm start, to start the script"
-
